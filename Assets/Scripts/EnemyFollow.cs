b44419751d7892d9a111c9e106910ef61b442bb3
m1kud4yo0 <162576517+m1kud4yo0@users.noreply.github.com>
2025-02-23 17:48:30 +0100

    insane threat hits plane

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -2,17 +2,38 @@ using UnityEngine;
 
 public class EnemyFollow : MonoBehaviour
 {
-    private Transform target;
+    public Transform target;
     public float speed = 6f;
+    private bool triggered = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        target = GameObject.FindGameObjectWithTag("PlayerBEAN").GetComponent<Transform>();
+        //target = GameObject.FindGameObjectWithTag("PlayerBEAN").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        if (triggered)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enemy Update Running");
+        if (other.CompareTag("PlayerTrigger"))
+        {
+            Debug.Log("Triggered");
+            triggered = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("PlayerTrigger"))
+        {
+            triggered = false;
+        }
     }
 }
