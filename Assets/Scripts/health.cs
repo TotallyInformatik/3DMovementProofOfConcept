a3578c5d05077e772b7ae167b4307d3e905fe9e7
m1kud4yo0 <162576517+m1kud4yo0@users.noreply.github.com>
2025-03-28 17:16:08 +0100

    attempt at health

---
+++ Assets/Scripts/health.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class health : MonoBehaviour
+{
+
+    public int hp = 3;
+    private bool triggered = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
